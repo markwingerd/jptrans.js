25990827eb6ae0434fcaef9b65d9ba98971cf2ac
Mark Wingerd <mawingerd@gmail.com>
2016-07-31 15:15:06 -0700

    Convert project into a module
    Changed the project so it works as a module rather than a set of
    functions and variables.  Found and fixes a few variables that
    were mistakenly added as globals.

--- spec/HiraganaSpec.js
+++ spec/HiraganaSpec.js
@@ -1,37 +1,37 @@
 describe("Hiragana", function() {
   it("handles basic Hiragana rules", function() {
-    expect(translate("mimasu")).toEqual("みます");
-    expect(translate("ocha")).toEqual("おちゃ");
+    expect(jptrans.translate("mimasu")).toEqual("みます");
+    expect(jptrans.translate("ocha")).toEqual("おちゃ");
   });
 
   it("handles the n character", function() {
-    expect(translate("mimasen")).toEqual("みません");
-    expect(translate("nyan")).toEqual("にゃん");
-    expect(translate("nihon")).toEqual("にほん")
+    expect(jptrans.translate("mimasen")).toEqual("みません");
+    expect(jptrans.translate("nyan")).toEqual("にゃん");
+    expect(jptrans.translate("nihon")).toEqual("にほん")
   });
 
   it("handles double O characters", function() {
-    expect(translate("Tookyoo")).toEqual("とうきょう");
+    expect(jptrans.translate("Tookyoo")).toEqual("とうきょう");
   });
 
   it("handles double consonants", function() {
-    expect(translate("matte")).toEqual("まって");
+    expect(jptrans.translate("matte")).toEqual("まって");
   });
 
   it("handles multiple words", function() {
-    expect(translate("watakushi no nihongo")).toEqual("わたくし の にほんご");
+    expect(jptrans.translate("watakushi no nihongo")).toEqual("わたくし の にほんご");
   });
 
   it("handles punctuation", function() {
-    expect(translate("Okaeri nasai.")).toEqual("おかえり なさい。");
+    expect(jptrans.translate("Okaeri nasai.")).toEqual("おかえり なさい。");
   });
 
   it("handles particles with special rules", function() {
-    expect(translate("Kuni wa Amerika desu.")).toEqual("くに は あめりか です。");
-    expect(translate("Chotto ocha o nomimasen ka?")).toEqual("ちょっと おちゃ を のみません か?");
+    expect(jptrans.translate("Kuni wa Amerika desu.")).toEqual("くに は あめりか です。");
+    expect(jptrans.translate("Chotto ocha o nomimasen ka?")).toEqual("ちょっと おちゃ を のみません か?");
   });
 
   it("fails gracefully", function() {
-    expect(translate("Konnifdschiwa")).toEqual("こんにFDSちわ");
+    expect(jptrans.translate("Konnifdschiwa")).toEqual("こんにFDSちわ");
   });
 });
